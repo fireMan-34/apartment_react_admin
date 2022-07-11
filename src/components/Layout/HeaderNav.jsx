ca810c9f08576b240a45b8cb06995e6290282f83
fireMan-34 <524915379@qq.com>
2022-07-11 19:36:51 +0800

    refactor: 💡 remeove shadow style and better function code

--- src/components/Layout/HeaderNav.jsx
+++ src/components/Layout/HeaderNav.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Menu, Avatar } from 'antd';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, } from 'react-redux';
 import { setCurMenu } from '../../store/commonSlice';
 
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import { getTransformMenu } from './commonFn';
+
 
 export default function HeaderNav() {
     const menu = useSelector(state => state.common.menu);
     const curMenu = useSelector(state => state.common.curMenu);
     const admin = useSelector(state => state.admin.adminInfo);
+    const naviagte = useNavigate();
+    const location = useLocation();
 
     const dispatch = useDispatch();
 
     const items = getTransformMenu(menu);
 
-    const clickItems = ({ key }) => {
+    const clickItems = useCallback(({ key }) => {
         dispatch(setCurMenu(key));
-    };
+        const urlOrNull = menu.find(menuItem => menuItem.name === key).url || null;
+        urlOrNull && urlOrNull !== location.pathname && naviagte(urlOrNull);
+    }, [menu, curMenu, admin, location]);
 
     return (
         <div className='headerNav'>
