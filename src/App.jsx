edae2a1ed57abe7b7af88a7dedd3e21dee976c42
fireMan-34 <524915379@qq.com>
2022-07-11 15:35:09 +0800

    feat: 🎸 layout 基础组件结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import 'antd/dist/antd.css'
+
 import store, { persistor } from './store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
-function App() {
-  console.log(store.getState());
 
+function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,23 @@
 import { Outlet } from 'react-router-dom';
+import { Layout } from 'antd';
 
-const Layout = () => {
-    return <>
-        common components
-        <Outlet />
-    </>
+import './index.scss';
+
+const { Header, Sider, Content } = Layout;
+
+//通过全局状态数据获取导航信息，而不依赖于页面具体传参
+
+const LayoutCom = () => {
+    return <Layout className='layout'>
+        <Sider>Sider</Sider>
+        <Layout>
+            <Header>
+                Header
+            </Header>
+            <Content>
+                Content
+            </Content>
+        </Layout>
+    </Layout>
 };
-export default Layout;
+export default LayoutCom;
