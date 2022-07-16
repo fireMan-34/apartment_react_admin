16b96926a629d3630e88f0e41c960e9727b70ba9
fireMan-34 <524915379@qq.com>
2022-07-16 22:17:34 +0800

    ci: 🎡 github aciton 重新学习

--- src/util/xlsxProvieder.js
+++ src/util/xlsxProvieder.js
@@ -24,11 +24,25 @@ export const xlsxProiver = ({ filename, data }) => {
  * 这个函数用来映射字段数组和源数据成为xlsx规范数据
  * @param {{title:string,dataIndex:string}[]} colums
  * @param {{key1:string|number|boolean}[]} sourceData
- * @param {?{text_true:string,text_false:string,dataIndex:string,title:string}} config
+ * @param {?{text_true:string,text_false:string,dataIndex:string,title:string,pipe:(key,value)=>value   }} config
  * @return {[colums,...string[][]]}
  */
 export const columsAndTableDataMapSheetData = function (colums, sourceData, config = {}) {
-    let { text_true = "是", text_false = "否", dataIndex = "dataIndex", title = "title" } = config;
+    let { text_true = "是", text_false = "否", dataIndex = "dataIndex", title = "title",
+        pipe = ({ key, value }) => {
+            switch (typeof value) {
+                case "undefined":
+                    return '';
+                case "object":
+                    if (value === null) return "";
+                    console.log(value, "是对象类型请知悉"); return "";
+                case "boolean":
+                    return value ? text_true : text_false;
+                default: return value;
+
+            }
+        }
+    } = config;
 
     const columnMapTitle = colums.map(colum => colum[title]);
     const columsMapDataIndex = colums.map(colum => colum[dataIndex]);
@@ -36,21 +50,8 @@ export const columsAndTableDataMapSheetData = function (colums, sourceData, conf
         throw new TypeError(`columns has some fields about label or string is not allow`);
     };
 
-    const everyObjectMapArray = sourceData.map(row => columsMapDataIndex.map(name => row[name]));
+    const everyObjectMapArray = sourceData.map(row => columsMapDataIndex.map(name => ({ key: name, value: row[name] })));
     const rowValueTranformToDefault = (pipe) => everyObjectMapArray.map(lineArr => lineArr.map(pipe));
-    const pipe = data => {
-        switch (typeof data) {
-            case "undefined":
-                return '';
-            case "object":
-                if (data === null) return "";
-                console.log(data, "是对象类型请知悉"); return "";
-            case "boolean":
-                return data ? text_true : text_false;
-            default: return data;
-
-        }
-    };
     return [columnMapTitle, ...rowValueTranformToDefault(pipe)];
 
 }
